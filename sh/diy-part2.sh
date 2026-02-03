79e717ad0860a26788051a46de41587d7197d5d8
daiwowanddd <409388913@qq.com>
2026-02-03 15:36:31 +0800

    bandix

--- sh/diy-part2.sh
+++ sh/diy-part2.sh
@@ -20,6 +20,6 @@ sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 git clone -b main --single-branch --filter=blob:none https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
-git clone -b main --single-branch --filter=blob:none https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+#git clone -b main --single-branch --filter=blob:none https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
 git clone -b main --single-branch --filter=blob:none https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
 #git clone -b main --single-branch --filter=blob:none https://github.com/nikkinikki-org/OpenWrt-nikki package/OpenWrt-nikki
